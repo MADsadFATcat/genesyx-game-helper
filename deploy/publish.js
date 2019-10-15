a523974dd50f374d2da8abff2f955efa562af9cf
Dmitriev Mikhail <dmitriev.dev@gmail.com>
2019-10-15 19:45:32 +0300

    patch

--- deploy/publish.js
+++ deploy/publish.js
@@ -1,6 +1,6 @@
 const webStore = require('./webStore');
 
-webStore.publish('trustedTesters').then(res2 => {
+webStore.publish().then(res2 => {
   console.log('Successfully published the newer version', res2);
 }).catch((error) => {
   console.log(`Error while publishing uploaded extension: ${error}`);
